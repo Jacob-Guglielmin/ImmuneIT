56432568782bff604d26ecd5321b375e2f12e7da
farazht <tehranifaraz314@gmail.com>
2024-03-23 21:17:55 -0700

    Update submit.js

--- js/submit.js
+++ js/submit.js
@@ -1,13 +1,17 @@
 document.getElementById("dbSubmit").addEventListener("click", function() {
-    var question = document.getElementById("dbQuestion").value,
-        answer1 = document.getElementById("dbAnswer1").value,
-        answer2 = document.getElementById("dbAnswer2").value,
-        answer3 = document.getElementById("dbAnswer3").value,
-        answer4 = document.getElementById("dbAnswer4").value,
-        correct = document.getElementById("dbCorrect").value,
-        category = document.getElementById("dbCategory").value,
-        postCorrect = document.getElementById("dbPostCorrect").value,
-        postIncorrect = document.getElementById("dbPostIncorrect").value;
+    var question = {
+        question: document.getElementById("dbQuestion").value,
+        answer1: document.getElementById("dbAnswer1").value,
+        answer2: document.getElementById("dbAnswer2").value,
+        answer3: document.getElementById("dbAnswer3").value,
+        answer4: document.getElementById("dbAnswer4").value,
+        correct: Number(document.getElementById("dbCorrect").value),
+        category: document.getElementById("dbCategory").value,
+        postCorrect: document.getElementById("dbPostCorrect").value,
+        postIncorrect: document.getElementById("dbPostIncorrect").value
+    };
+
+    // put question object into firebase
 
     document.getElementById("dbQuestion").value = "";
     document.getElementById("dbAnswer1").value = "";
@@ -18,6 +22,4 @@ document.getElementById("dbSubmit").addEventListener("click", function() {
     document.getElementById("dbCategory").value = "";
     document.getElementById("dbPostCorrect").value = "";
     document.getElementById("dbPostIncorrect").value = "";
-
-    // add to firebase (todo)
 });

cb062f3d3df11588e659c6d8119d5826367e033d
Marecu <marcusthoen27@gmail.com>
2024-03-23 18:58:55 -0700

    More changes to question + updated company name

--- js/question.js
+++ js/question.js
@@ -1,17 +1,21 @@
 class Question {
-    constructor(question, o1, o2, o3, o4, correct, category) {
+    constructor(question, o1, o2, o3, o4, correct, category, pc, pi) {
         this.question = question;
         this.option1 = o1;
         this.option2 = o2;
         this.option3 = o3;
         this.option4 = o4;
-        this.correct = correct;
+        this.correct = correct; // Uses 1-based indexing
         this.category = category;
+        this.postCorrect = pc;
+        this.postIncorrect = pi;
     }
 }
 
-let testQ = new Question("what is love? what is love? what is love? what is love? what is love? what is love? what is love? what is love? what is love? what is love? what is love? what is love? ", "option 1", "option 2", "option 3", "option 4", 2)
+let testQ = new Question("what is love? ", "option 1", "option 2", "option 3", "option 4", 2, "curling", "YOOO LETS GO U GOT IT RIGHT", "dumbass");
 
+
+let currentQuestion = testQ;
 
 // Takes in a Question object pertaining to the question
 // Returns a div containing the question
@@ -54,11 +58,19 @@ const makeQuestion = (content) => {
 
 const clickHandler = (option) => {
     let clicked = document.getElementsByClassName("qOption")[option];
+    for (let option of document.getElementsByClassName("qOption")) {
+        option.removeAttribute("onclick");
+        option.classList.remove("hoverEffect");
+        option.classList.add("nonSelected");
+    }
     if (clicked.classList.contains("correct")) {
         clicked.classList.add("correctSelected");
+        document.getElementsByClassName("qQuestion")[0].innerHTML = currentQuestion.postCorrect;
     } else {
         clicked.classList.add("incorrectSelected");
+        document.getElementsByClassName("qQuestion")[0].innerHTML = currentQuestion.postIncorrect;
     }
+    
 }
 
 document.getElementById("interfaceContainer").appendChild(makeQuestion(testQ));

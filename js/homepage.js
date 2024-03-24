f37afe69ef7f3c8d73f8df8599db9d3ffa58f15d
Jacob-Guglielmin <jacobguglielmin@gmail.com>
2024-03-23 20:52:02 -0700

    Homepage done??

--- js/homepage.js
+++ js/homepage.js
@@ -1,13 +1,15 @@
 const employeeNameElement = document.getElementById("employeeName");
 const skillsChartElement = document.getElementById("skillsChart");
+const progressElement = document.getElementById("amountDone");
 
-const employeeID = 0;
+const employeeID = 1;
 
 fetch("./employees.json").then((data) => {
     return data.json();
 }).then((data) => {
     employeeNameElement.innerText = data.employees[employeeID].name;
 
+    let total = 0;
     for (let skill in data.employees[employeeID].skills) {
         console.log(skill);
         const skillElement = document.createElement("div");
@@ -22,5 +24,8 @@ fetch("./employees.json").then((data) => {
         skillBar.style.height = (data.employees[employeeID].skills[skill] * (97/100)) + "%";
 
         skillsChart.appendChild(skillElement);
+
+        total += data.employees[employeeID].skills[skill];
     }
+    progressElement.innerText = Math.floor((total / Object.keys(data.employees[employeeID].skills).length)) + "%";
 });
